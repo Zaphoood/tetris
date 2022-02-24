c054c614c9755ae3434728864cfdb631f194b709
Mathis Weber <mathis.c.weber@web.de>
2022-02-24 21:57:52 +0100

    Draw grid and contents in Playfield
    Iterate over the grid, draw a rectangle at each location using the
    corresponding color.

--- src/playfield.cpp
+++ src/playfield.cpp
@@ -1,15 +1,48 @@
 #include "playfield.h"
 #include "SDL.h"
+#include "iostream"
 
-Playfield::Playfield() {}
+Playfield::Playfield() {
+    // Initialize all cells to zero
+    for (int i = 0; i < GRID_SIZE_Y; i++) {
+        for (int j = 0; j < GRID_SIZE_X; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    // Set bottom right cell to 1
+    grid[39][0] = 1;
+}
 
 void Playfield::draw(SDL_Renderer *renderer, int pos_x, int pos_y) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    for (int row = 0; row < GRID_SIZE_Y; row++) {
+    drawGrid(renderer);
+    for (int row = GRID_START_Y; row < GRID_SIZE_Y; row++) {
         for (int col = 0; col < GRID_SIZE_X; col++) {
-            SDL_Rect rect{col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE,
-                          CELL_SIZE};
-            SDL_RenderDrawRect(renderer, &rect);
+            SDL_Rect rect{col * CELL_SIZE, (row - GRID_START_Y) * CELL_SIZE,
+                          CELL_SIZE, CELL_SIZE};
+            if (grid[row][col] > 0) {
+                // Draw a square with the color according to the current Mino
+                SDL_SetRenderDrawColor(renderer, COLORS[grid[row][col]][0],
+                                       COLORS[grid[row][col]][1],
+                                       COLORS[grid[row][col]][2], 255);
+                SDL_RenderFillRect(renderer, &rect);
+                // Draw a black outline
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderDrawRect(renderer, &rect);
+            }
         }
+    }
+}
+
+void Playfield::drawGrid(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, GRID_COLOR[0], GRID_COLOR[1],
+                           GRID_COLOR[2], 255);
+    // Draw horizontal lines
+    for (int y = GRID_SIZE_VISIBLE_Y - 1; y > 0; y--) {
+        SDL_RenderDrawLine(renderer, 0, y * CELL_SIZE, WINDOW_X, y * CELL_SIZE);
+    }
+    // Draw vertical lines
+    for (int x = GRID_SIZE_X - 1; x > 0; x--) {
+        SDL_RenderDrawLine(renderer, x * CELL_SIZE, 0, x * CELL_SIZE, WINDOW_Y);
     }
 }
--- src/playfield.h
+++ src/playfield.h
@@ -3,11 +3,22 @@
 inline const int CELL_SIZE = 20;
 inline const int GRID_SIZE_X = 10;
 inline const int GRID_SIZE_Y = 40;
+inline const int WINDOW_X = GRID_SIZE_X * CELL_SIZE;
+inline const int WINDOW_Y = GRID_SIZE_Y * CELL_SIZE;
 inline const int GRID_SIZE_VISIBLE_X = GRID_SIZE_X;
 inline const int GRID_SIZE_VISIBLE_Y = 20;
+inline const int GRID_START_Y = GRID_SIZE_Y - GRID_SIZE_VISIBLE_Y;
+
+inline const uint8_t GRID_COLOR[3] = {200, 200, 200};
+inline const uint8_t COLORS[][3] = {
+    {255, 255, 255}, // White
+    {0, 255, 255},   // I: Cyan
+    {255, 255, 0}    // O: Yellow
+};
 
 class Playfield {
-    int grid[GRID_SIZE_X][GRID_SIZE_Y];
+    uint8_t grid[GRID_SIZE_Y][GRID_SIZE_X];
+    void drawGrid(SDL_Renderer *renderer);
 
   public:
     Playfield();
