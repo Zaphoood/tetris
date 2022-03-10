c56247a80edcaba9639a4ae5159c3f6a4a790de7
Mathis Weber <mathis.c.weber@web.de>
2022-03-10 22:22:16 +0100

    Stop tracking libs folder

--- libs/SDL2/build/.libs/SDL_crc32.d
+++
@@ -1,8 +0,0 @@
-build/SDL_crc32.lo: \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/stdlib/SDL_crc32.c \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/stdlib/../SDL_internal.h \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/stdlib/../dynapi/SDL_dynapi.h \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_config.h \
- include/SDL_log.h include/SDL_stdinc.h include/SDL_stdinc.h
--- libs/SDL2/build/.libs/SDL_error.d
+++
@@ -1,9 +0,0 @@
-build/SDL_error.lo: \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/SDL_error.c \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/./SDL_internal.h \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/./dynapi/SDL_dynapi.h \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/./dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_config.h \
- include/SDL_log.h include/SDL_stdinc.h include/SDL_error.h \
- /home/mathis/projects/tetris/libs/SDL2-2.0.20/src/SDL_error_c.h
